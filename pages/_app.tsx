c9889a2b68e77b9e0ff26c4d6fa580fa5bb188e3
Sha Mwe La <shamwela2002@gmail.com>
2021-09-01 18:37:35 +0630

    Minor fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import 'tailwindcss/tailwind.css'
-import "styles/globals.css"
+import 'styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
