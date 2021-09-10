b1313548f0bc68765b4d4a148d227c923c56d82e
Sha Mwe La <shamwela2002@gmail.com>
2021-09-10 19:31:51 +0630

    Minor edit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx$/,
+})
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+})
+
 module.exports = {
   i18n: {
     locales: ['en'],
