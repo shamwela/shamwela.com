dc4dc4ebb893626c329214b9fd512914f3fabb1a
Sha Mwe La <shamwela2002@gmail.com>
2021-09-10 20:33:58 +0630

    Remove unused code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,3 @@
-import { IconContext } from 'react-icons'
 import {
   AiOutlineMail,
   AiFillGithub,
@@ -12,43 +11,41 @@ const Footer = () => {
       style={{ maxWidth: '70ch', minHeight: '10vh' }}
       className='mx-auto px-8 py-4 flex justify-evenly items-center'
     >
-      <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
-        <a
-          aria-label='Email'
-          href='mailto:shamwela@hotmail.com'
-          target='_blank'
-          rel='noopener'
-        >
-          <AiOutlineMail />
-        </a>
+      <a
+        aria-label='Email'
+        href='mailto:shamwela@hotmail.com'
+        target='_blank'
+        rel='noopener'
+      >
+        <AiOutlineMail />
+      </a>
 
-        <a
-          aria-label='GitHub'
-          href='https://github.com/shamwela'
-          target='_blank'
-          rel='noopener'
-        >
-          <AiFillGithub />
-        </a>
+      <a
+        aria-label='GitHub'
+        href='https://github.com/shamwela'
+        target='_blank'
+        rel='noopener'
+      >
+        <AiFillGithub />
+      </a>
 
-        <a
-          aria-label='LinkedIn'
-          href='https://www.linkedin.com/in/shamwela'
-          target='_blank'
-          rel='noopener'
-        >
-          <AiFillLinkedin />
-        </a>
+      <a
+        aria-label='LinkedIn'
+        href='https://www.linkedin.com/in/shamwela'
+        target='_blank'
+        rel='noopener'
+      >
+        <AiFillLinkedin />
+      </a>
 
-        <a
-          aria-label='Twitter'
-          href='https://twitter.com/shamwela_'
-          target='_blank'
-          rel='noopener'
-        >
-          <AiOutlineTwitter />
-        </a>
-      </IconContext.Provider>
+      <a
+        aria-label='Twitter'
+        href='https://twitter.com/shamwela_'
+        target='_blank'
+        rel='noopener'
+      >
+        <AiOutlineTwitter />
+      </a>
     </footer>
   )
 }
