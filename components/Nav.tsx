b8347858c5c526a13848c58c9dd965e5e0414cc0
Sha Mwe La <shamwela2002@gmail.com>
2021-09-11 20:19:43 +0630

    Create Nav

---
+++ components/Nav.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+const Nav = () => {
+  return (
+    <nav>
+      {/* Height should be the same as the Layout's bottom padding */}
+      <ul className='list-none h-12 bg-secondary fixed md:static right-0 bottom-0 left-0 flex justify-evenly items-center z-50'>
+        <li>
+          <Link href='/'>
+            <a className='no-underline text-primary'>About Me</a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href='/articles'>
+            <a className='no-underline text-primary'>Articles</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
