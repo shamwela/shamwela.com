91eda82e15fb5cd6acec05881db152448edeb98f
Sha Mwe La <shamwela2002@gmail.com>
2021-09-13 19:46:15 +0630

    Edit font weights

--- components/BlockAnchor.tsx
+++ components/BlockAnchor.tsx
@@ -14,7 +14,7 @@ const BlockAnchor = ({
       href={href}
       target={target}
       rel={rel}
-      className='px-6 py-2 no-underline block w-max bg-secondary text-primary'
+      className='px-6 py-2 no-underline block w-max bg-secondary text-primary font-bold'
     >
       {children}
     </a>
