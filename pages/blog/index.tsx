3ff0ebb8b96a7b50b41da6c0e8d35a1e5aa00d2c
Sha Mwe La <shamwela2002@gmail.com>
2021-09-16 19:29:20 +0630

    Add the new blog post to the /blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -13,6 +13,10 @@ const Blog = () => {
       <Layout>
         <h1>Blog</h1>
 
+        <Link href='/blog/why-i-love-tailwind-css'>
+          <a>Why I Love Tailwind CSS</a>
+        </Link>
+
         <Link href='/blog/roadmap'>
           <a>Web Development Roadmap for Beginners</a>
         </Link>
