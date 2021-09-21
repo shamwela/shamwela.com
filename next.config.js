dadc0bf1b8530bc29ba7916aa79b29c22d75408d
Sha Mwe La <shamwela2002@gmail.com>
2021-09-21 12:02:07 +0630

    Move lang attribute into _document.tsx

--- next.config.js
+++ next.config.js
@@ -4,10 +4,4 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-
-  // Add lang='en' to all pages
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
 })
