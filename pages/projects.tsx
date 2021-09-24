6d3a62e53c96baf820c02d12161cfd846fbc3879
Sha Mwe La <shamwela2002@gmail.com>
2021-09-24 12:42:11 +0630

    Add projects page using GitHub and GraphQL

---
+++ pages/projects.tsx
@@ -0,0 +1,80 @@
+import Head from 'next/head'
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  gql,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const Projects = ({ pinnedItems }) => {
+  return (
+    <>
+      <Head>
+        <title>Projects | Sha Mwe La</title>
+        <meta name='description' content="Sha Mwe La's Projects" />
+      </Head>
+
+      {pinnedItems.map((item) => {
+        const { id, url, name, description } = item
+
+        return (
+          <div key={id}>
+            <a href={url}>{name}</a>
+            {description && <p>{description}</p>}
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Projects
+
+export async function getStaticProps() {
+  const httpLink = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    }
+  })
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  })
+
+  const { data } = await client.query({
+    query: gql`
+      {
+        user(login: "shamwela") {
+          pinnedItems(first: 6) {
+            edges {
+              node {
+                ... on Repository {
+                  id
+                  url
+                  name
+                  description
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  })
+
+  const { user } = data
+  const pinnedItems = user.pinnedItems.edges.map(({ node }) => node)
+
+  return {
+    props: { pinnedItems },
+  }
+}
