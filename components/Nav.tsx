703d5e54722124592f9fc4948e61e74525ab2b54
Sha Mwe La <shamwela2002@gmail.com>
2021-10-01 15:24:23 +0630

    Put <nav> always on top

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav>
       {/* Height should be the same as the Layout's bottom padding */}
-      <ol className='list-none h-12 bg-secondary fixed md:static right-0 bottom-0 left-0 flex justify-center items-center gap-x-8 z-50'>
+      <ol className='list-none h-12 bg-secondary flex justify-center items-center gap-x-8'>
         <li>
           <Link href='/'>
             <a className='no-underline text-primary'>About Me</a>
