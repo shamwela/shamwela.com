802bbea0d6d34d281e9bbc4fb3a366dcd329e58a
Sha Mwe La <shamwela2002@gmail.com>
2021-10-06 20:30:19 +0630

    Add bottom margin to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,8 @@ const Footer = () => {
   return (
     <footer
       style={{ maxWidth: '70ch', minHeight: '10vh' }}
-      className='mx-auto flex justify-evenly items-center'
+      // Bottom margin should be the same as the nav height
+      className='mb-14 mx-auto flex justify-evenly items-center'
     >
       <a
         aria-label='Email'
