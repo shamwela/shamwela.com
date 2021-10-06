7a87cfc39c0b6a310ceabcd14ab94648bb29a3e0
Sha Mwe La <shamwela2002@gmail.com>
2021-10-06 20:06:55 +0630

    Add missing "/" in blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -11,23 +11,23 @@ const Blog = () => {
 
       <h1>Blog</h1>
 
-      <Link href='blog/tips-for-coders'>
+      <Link href='/blog/tips-for-coders'>
         <a>Tips for Coders</a>
       </Link>
 
-      <Link href='blog/roadmap'>
+      <Link href='/blog/roadmap'>
         <a>Web Development Roadmap for Beginners</a>
       </Link>
 
-      <Link href='blog/favorite-coding-resources'>
+      <Link href='/blog/favorite-coding-resources'>
         <a>Favorite Coding Resources</a>
       </Link>
 
-      <Link href='blog/favorite-visual-studio-code-extensions'>
+      <Link href='/blog/favorite-visual-studio-code-extensions'>
         <a>Favorite Visual Studio Code Extensions</a>
       </Link>
 
-      <Link href='blog/why-i-love-tailwind-css'>
+      <Link href='/blog/why-i-love-tailwind-css'>
         <a>Why I Love Tailwind CSS</a>
       </Link>
     </>
