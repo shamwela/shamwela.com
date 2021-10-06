808c512d8e65a16efd5124659431afde49b64f5b
Sha Mwe La <shamwela2002@gmail.com>
2021-10-06 20:05:54 +0630

    Implement nextBlog anchor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,22 @@ import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 import Head from 'next/head'
 import Nav from 'components/Nav'
+import Link from 'next/link'
 import Footer from 'components/Footer'
 import 'tailwindcss/tailwind.css'
 import 'styles/globals.css'
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter()
+
+  const blogSlugs = [
+    'tips-for-coders',
+    'roadmap',
+    'favorite-coding-resources',
+    'favorite-visual-studio-code-extensions',
+    'why-i-love-tailwind-css',
+  ]
+
   useEffect(() => {
     const handleRouteChange = (url) => {
       gtag.pageview(url)
@@ -19,11 +29,26 @@ const App = ({ Component, pageProps }) => {
     }
   }, [router.events])
 
+  const separatedParts = router.asPath.split('/')
+  const currentBlog = separatedParts[separatedParts.length - 1]
+  const currentBlogIndex = blogSlugs.indexOf(currentBlog)
+
+  const nextBlog = blogSlugs[currentBlogIndex + 1]
+  const previousBlog = blogSlugs[currentBlogIndex - 1]
+
   return (
     <>
       <Head>
         <link rel='icon' href='/assets/favicon.ico' />
       </Head>
+
+      {router.asPath.includes('/blog/') && nextBlog && (
+        <div>
+          <Link href={'/blog/' + nextBlog}>
+            <a>Next Blog</a>
+          </Link>
+        </div>
+      )}
 
       <Nav />
 
