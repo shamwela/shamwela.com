c1fa914c0c85d4a89464892659da2836d97bbc1e
Sha Mwe La <shamwela2002@gmail.com>
2021-10-06 20:39:33 +0630

    Make a redirect for the "/" page

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog/tips-for-coders',
+        permanent: true,
+      },
+    ]
+  },
 })
