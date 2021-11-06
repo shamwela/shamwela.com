facf9a7e32fc5d0e3a20bcbadae829f9422e36a8
Sha Mwe La <shamwela2002@gmail.com>
2021-11-06 15:11:56 +0630

    Test meta tags

--- components/Head.tsx
+++ components/Head.tsx
@@ -14,14 +14,17 @@ const CustomHead = ({
     <Head>
       <title>{title}</title>
       <meta property='og:title' content={title} />
+      <meta />
 
       <meta name='description' content={description} />
       <meta property='og:description' content={description} />
 
       <meta property='og:image' content={ogImageUrl} />
+      <meta name="twitter:image" content={ogImageUrl}/>
+
       <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:site' content='@shamwela_' />
       <meta name='twitter:creator' content='@shamwela_' />
+      <meta name='twitter:site' content='@shamwela_' />
     </Head>
   )
 }
