fd1b58e3366e0e174cdc3d812b4362a877565323
Sha Mwe La <shamwela2002@gmail.com>
2021-11-21 14:55:05 +0630

    Add prismjs for syntax highlighting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
+import prism from 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css'
 import Head from 'next/head'
 import BlogCallToAction from 'components/BlogCallToAction'
 import Footer from 'components/Footer'
@@ -20,6 +22,10 @@ const App = ({ Component, pageProps }) => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
   }, [router.events])
+
+  useEffect(() => {
+    prism.highlightAll()
+  }, [])
 
   return (
     <>
