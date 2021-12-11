bc73994d9c911e364995e2b5d4eb348585984252
Sha Mwe La <shamwela2002@gmail.com>
2021-12-11 21:50:32 +0630

    Delete NavLink active style

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,18 +1,10 @@
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
 const NavLink = ({ href, children }: { href: string; children: string }) => {
-  const { pathname } = useRouter()
-  const isActive = pathname.includes(href)
-
   return (
     <li>
       <Link href={href}>
-        <a
-          className={`${
-            isActive ? 'underline' : 'no-underline'
-          } px-6 py-2 rounded-full font-medium text-primary`}
-        >
+        <a className={'no-underline px-6 py-2 rounded-full text-primary'}>
           {children}
         </a>
       </Link>
