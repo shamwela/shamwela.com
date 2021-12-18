7722a0eeaac939604e545040b7d55583d484ebfb
Sha Mwe La <shamwela2002@gmail.com>
2021-12-18 18:42:05 +0630

    Improve <nav> tag

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,14 +2,11 @@ import NavLink from './NavLink'
 
 const Nav = () => {
   return (
-    <nav className='bg-secondary'>
-      <ol
-        className='flex items-center mx-auto list-none h-14 justify-evenly'
-        style={{ maxWidth: '70ch' }}
-      >
-        <NavLink href='/'>About</NavLink>
-        <NavLink href='/projects'>Projects</NavLink>
-        <NavLink href='/blog'>Blog</NavLink>
+    <nav>
+      <ol className='max-w-[70ch] flex items-center mx-auto list-none h-14 justify-evenly'>
+        <NavLink text='About' href='/' />
+        <NavLink text='Projects' href='/projects' />
+        <NavLink text='Blog' href='/blog' hasNestedRoutes />
       </ol>
     </nav>
   )
--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,11 +1,34 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-const NavLink = ({ href, children }: { href: string; children: string }) => {
+const NavLink = ({
+  text,
+  href,
+  hasNestedRoutes = false,
+}: {
+  text: string
+  href: string
+  hasNestedRoutes?: boolean
+}) => {
+  const { pathname } = useRouter()
+  let isActive: boolean
+
+  if (hasNestedRoutes) {
+    // For example, /blog, /blog/1, /blog/2
+    isActive = pathname.startsWith(href)
+  } else {
+    isActive = pathname === href
+  }
+
   return (
     <li>
       <Link href={href}>
-        <a className={'no-underline px-6 py-2 rounded-full text-primary'}>
-          {children}
+        <a
+          className={`${
+            isActive && 'text-accent'
+          } no-underline px-6 py-2 rounded-full`}
+        >
+          {text}
         </a>
       </Link>
     </li>
