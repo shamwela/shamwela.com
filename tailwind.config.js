1e4c725802ba8b74cd7a691185703f8b0c6b8207
Sha Mwe La <shamwela2002@gmail.com>
2021-12-18 21:00:52 +0630

    Improve Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#181818',
-        secondary: '#fff',
-        accent: '#38aecc',
-      },
-      fontFamily: {
-        sans: 'Roboto, Helvetica, sans-serif',
-        // "!important" is used here to override Prism.js default font family
-        mono: '"Roboto Mono", Consolas, monospace !important',
-      },
+    colors: {
+      primary: '#181818',
+      secondary: '#fff',
+      accent: '#38aecc',
+    },
+    fontFamily: {
+      sans: 'Roboto, Helvetica, sans-serif',
+      mono: '"Roboto Mono", Consolas, monospace !important',
+      // "!important" is used here to override Prism.js default font family
     },
   },
 }
