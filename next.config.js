03fc73aefa9176f84ae3826b46174ebb4fed60b7
Sha Mwe La <shamwela2002@gmail.com>
2021-12-19 11:41:42 +0630

    In next.config.js, add some comments

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx$/,
 })
 
+// Wrap the config so it can add necessary configurations to process MDX files.
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  // Tell Next.js that it should render MDX files as page if they're in the page directory.
 })
