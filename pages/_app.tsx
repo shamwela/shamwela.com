abe396c517fb633f1a7b19db489dd5144c0f16d7
Sha Mwe La <shamwela2002@gmail.com>
2021-12-20 15:08:13 +0630

    Delete code for prism.js because I no longer use it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,8 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import Footer from 'components/Footer'
 import Nav from 'components/Nav'
+import Footer from 'components/Footer'
 import 'styles/globals.css'
-import prism from 'prismjs'
-import 'prismjs/themes/prism-tomorrow.css'
-// Some languages are not added by default. They should be added manually here.
-import 'prismjs/components/prism-sass'
-import 'prismjs/components/prism-jsx'
-import 'prismjs/components/prism-tsx'
-import 'prismjs/components/prism-git'
 
 const App = ({ Component, pageProps }) => {
-  const { pathname } = useRouter()
-
-  useEffect(() => {
-    // Only if it is a blog page, highlight the <code> tags
-    if (pathname.startsWith('/blog/')) {
-      prism.highlightAll()
-    }
-  }, [pathname])
-
   return (
     <>
       <Nav />
