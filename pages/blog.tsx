c7eeeb84a741aeaf82ec3a10cc8880e282396566
Sha Mwe La <shamwela2002@gmail.com>
2021-12-20 20:46:13 +0630

    Implement sorting and searching

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,13 +3,21 @@ import { allBlogs } from '.contentlayer/data'
 import { pick } from 'lib/utils'
 import Link from 'next/link'
 import Head from 'components/Head'
+import { useState } from 'react'
 
 export const getStaticProps = async () => {
-  const posts = allBlogs.map((post) => pick(post, ['slug', 'title']))
-  return { props: { posts } }
+  const blogs = allBlogs.map((blog) => pick(blog, ['slug', 'title', 'date']))
+  return { props: { blogs } }
 }
 
-const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Blog = ({ blogs }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [searchValue, setSearchValue] = useState('')
+  const filteredBlogPosts = blogs
+    .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
+    .filter((blog) =>
+      blog.title.toLowerCase().includes(searchValue.toLowerCase())
+    )
+
   return (
     <>
       <Head
@@ -18,8 +26,16 @@ const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
         image='/images/sha-mwe-la-open-graph.png'
         date='2021-12-20'
       />
+
       <h1>Blog</h1>
-      {posts.map(({ slug, title }) => (
+
+      <input
+        type='search'
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.currentTarget.value)}
+      />
+
+      {filteredBlogPosts.map(({ slug, title }) => (
         <Link href={'/blog/' + slug} key={slug}>
           <a>{title}</a>
         </Link>
