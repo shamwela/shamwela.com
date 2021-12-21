abaec05a4bfc934a49a65530a642e5e4b92b31b7
Sha Mwe La <shamwela2002@gmail.com>
2021-12-21 12:41:14 +0630

    Add Nav Links in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,39 @@
+import Link from 'next/link'
+
 const Footer = () => {
   return (
-    <footer className='max-w-[70ch] min-h-[10vh] flex items-center mx-auto justify-evenly'>
-      <a href='mailto:shamwela@hotmail.com' target='_blank' rel='noreferrer'>
-        Email
-      </a>
+    <footer className='max-w-[70ch] mx-auto grid gap-y-4 md:grid-cols-2 p-5 border-t-2'>
+      <div className='flex flex-col gap-y-4'>
+        <Link href='/'>
+          <a>About</a>
+        </Link>
+        <Link href='/blog'>
+          <a>Blog</a>
+        </Link>
+      </div>
 
-      <a href='https://github.com/shamwela' target='_blank' rel='noreferrer'>
-        GitHub
-      </a>
-
-      <a
-        href='https://www.linkedin.com/in/shamwela'
-        target='_blank'
-        rel='noreferrer'
-      >
-        LinkedIn
-      </a>
-
-      <a href='https://twitter.com/shamwela_' target='_blank' rel='noreferrer'>
-        Twitter
-      </a>
+      <div className='flex flex-col gap-y-4'>
+        <a href='mailto:shamwela@hotmail.com' target='_blank' rel='noreferrer'>
+          Email
+        </a>
+        <a href='https://github.com/shamwela' target='_blank' rel='noreferrer'>
+          GitHub
+        </a>
+        <a
+          href='https://www.linkedin.com/in/shamwela'
+          target='_blank'
+          rel='noreferrer'
+        >
+          LinkedIn
+        </a>
+        <a
+          href='https://twitter.com/shamwela_'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Twitter
+        </a>
+      </div>
     </footer>
   )
 }
