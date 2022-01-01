8f83df5cb517e3bcd195a53cd62ad5cf8ee17b1e
Sha Mwe La <shamwela2002@gmail.com>
2022-01-01 19:43:16 +0630

    Improve blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,76 +1,45 @@
 import { getAllPostsMeta } from 'lib/mdx'
 import type { PostMeta } from 'types/post'
+import Link from 'next/link'
+import Head from 'components/Head'
+import { useState } from 'react'
 
-export function getStaticProps() {
+export const getStaticProps = async () => {
   const posts = getAllPostsMeta()
   return { props: { posts } }
 }
 
 const Blog = ({ posts }: { posts: PostMeta[] }) => {
+  const [searchValue, setSearchValue] = useState('')
+  const filteredBlogs = posts.filter((blog) =>
+    blog.title.toLowerCase().includes(searchValue.toLowerCase())
+  )
+
   return (
     <>
+      <Head
+        title="Sha Mwe La's Blog"
+        description="Sha Mwe La's Blog"
+        image='/images/sha-mwe-la-open-graph.png'
+        date='2021-12-20'
+      />
       <h1>Blog</h1>
-      <div className='mt-12 space-y-10'>
-        {posts.map((post) => (
-          <div key={post.slug}>
-            <a href={`/blog/${post.slug}`}>{post.title}</a>
-          </div>
-        ))}
-      </div>
+      <input
+        placeholder='Search'
+        aria-label='Search'
+        type='search'
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.currentTarget.value)}
+      />
+      {filteredBlogs.length === 0 && <p>No blogs found.</p>}
+
+      {filteredBlogs.map(({ slug, title }) => (
+        <Link href={'/blog/' + slug} key={slug}>
+          <a>{title}</a>
+        </Link>
+      ))}
     </>
   )
 }
 
 export default Blog
-
-// import { InferGetStaticPropsType } from 'next'
-// import { allBlogs } from '.contentlayer/data'
-// import { pick } from 'lib/utils'
-// import Link from 'next/link'
-// import Head from 'components/Head'
-// import { useState } from 'react'
-
-// export const getStaticProps = async () => {
-//   const blogs = allBlogs
-//     .map((blog) => pick(blog, ['slug', 'title', 'date']))
-//     .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
-//   return { props: { blogs } }
-// }
-
-// const Blog = ({ blogs }: InferGetStaticPropsType<typeof getStaticProps>) => {
-//   const [searchValue, setSearchValue] = useState('')
-//   const filteredBlogs = blogs.filter((blog) =>
-//     blog.title.toLowerCase().includes(searchValue.toLowerCase())
-//   )
-
-//   return (
-//     <>
-//       <Head
-//         title="Sha Mwe La's Blog"
-//         description="Sha Mwe La's Blog"
-//         image='/images/sha-mwe-la-open-graph.png'
-//         date='2021-12-20'
-//       />
-
-//       <h1>Blog</h1>
-
-//       <input
-//         placeholder='Search'
-//         aria-label='Search'
-//         type='search'
-//         value={searchValue}
-//         onChange={(event) => setSearchValue(event.currentTarget.value)}
-//       />
-
-//       {filteredBlogs.length === 0 && <p>No blogs found.</p>}
-
-//       {filteredBlogs.map(({ slug, title }) => (
-//         <Link href={'/blog/' + slug} key={slug}>
-//           <a>{title}</a>
-//         </Link>
-//       ))}
-//     </>
-//   )
-// }
-
-// export default Blog
