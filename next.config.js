b97d7eaa84402ab0976d85fc43bdd7e4196401cd
Sha Mwe La <shamwela2002@gmail.com>
2022-01-01 18:50:24 +0630

    Delete previous MDX setup

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const { withContentlayer } = require('next-contentlayer')
-
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = withContentlayer()({
-  swcMinify: true,
-  reactStrictMode: true,
-})
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,51 +1,53 @@
-import { InferGetStaticPropsType } from 'next'
-import { allBlogs } from '.contentlayer/data'
-import { pick } from 'lib/utils'
-import Link from 'next/link'
-import Head from 'components/Head'
-import { useState } from 'react'
-
-export const getStaticProps = async () => {
-  const blogs = allBlogs
-    .map((blog) => pick(blog, ['slug', 'title', 'date']))
-    .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
-  return { props: { blogs } }
-}
-
-const Blog = ({ blogs }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [searchValue, setSearchValue] = useState('')
-  const filteredBlogs = blogs.filter((blog) =>
-    blog.title.toLowerCase().includes(searchValue.toLowerCase())
-  )
-
-  return (
-    <>
-      <Head
-        title="Sha Mwe La's Blog"
-        description="Sha Mwe La's Blog"
-        image='/images/sha-mwe-la-open-graph.png'
-        date='2021-12-20'
-      />
-
-      <h1>Blog</h1>
-
-      <input
-        placeholder='Search'
-        aria-label='Search'
-        type='search'
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.currentTarget.value)}
-      />
-
-      {filteredBlogs.length === 0 && <p>No blogs found.</p>}
-
-      {filteredBlogs.map(({ slug, title }) => (
-        <Link href={'/blog/' + slug} key={slug}>
-          <a>{title}</a>
-        </Link>
-      ))}
-    </>
-  )
-}
-
+const Blog = () => <>Blog</>
 export default Blog
+// import { InferGetStaticPropsType } from 'next'
+// import { allBlogs } from '.contentlayer/data'
+// import { pick } from 'lib/utils'
+// import Link from 'next/link'
+// import Head from 'components/Head'
+// import { useState } from 'react'
+
+// export const getStaticProps = async () => {
+//   const blogs = allBlogs
+//     .map((blog) => pick(blog, ['slug', 'title', 'date']))
+//     .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
+//   return { props: { blogs } }
+// }
+
+// const Blog = ({ blogs }: InferGetStaticPropsType<typeof getStaticProps>) => {
+//   const [searchValue, setSearchValue] = useState('')
+//   const filteredBlogs = blogs.filter((blog) =>
+//     blog.title.toLowerCase().includes(searchValue.toLowerCase())
+//   )
+
+//   return (
+//     <>
+//       <Head
+//         title="Sha Mwe La's Blog"
+//         description="Sha Mwe La's Blog"
+//         image='/images/sha-mwe-la-open-graph.png'
+//         date='2021-12-20'
+//       />
+
+//       <h1>Blog</h1>
+
+//       <input
+//         placeholder='Search'
+//         aria-label='Search'
+//         type='search'
+//         value={searchValue}
+//         onChange={(event) => setSearchValue(event.currentTarget.value)}
+//       />
+
+//       {filteredBlogs.length === 0 && <p>No blogs found.</p>}
+
+//       {filteredBlogs.map(({ slug, title }) => (
+//         <Link href={'/blog/' + slug} key={slug}>
+//           <a>{title}</a>
+//         </Link>
+//       ))}
+//     </>
+//   )
+// }
+
+// export default Blog
