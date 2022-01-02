8e2140202c1c91df367a8f8b5b156b27ee623044
Sha Mwe La <shamwela2002@gmail.com>
2022-01-02 11:23:06 +0630

    Add "ui" folder to Tailwind's content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './ui/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
