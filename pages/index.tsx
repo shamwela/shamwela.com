5e9c0ead886fa9f3b5a3de0c90deffa8c92bba3c
Sha Mwe La <shamwela2002@gmail.com>
2022-04-18 22:49:44 +0630

    Improve

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,26 @@
 import Head from 'components/Head'
 import Image from 'next/image'
 
-const Home = () => {
-  return (
-    <>
-      <Head title='About Sha Mwe La' />
-      <p>
-        Hi, I'm Sha Mwe La. I write programs. I love listening to music and
-        watching movies.
-      </p>
-      <div className='self-start'>
-        <Image
-          alt='Sha Mwe La'
-          src='/images/sha-mwe-la-photo.jpg'
-          blurDataURL='/images/sha-mwe-la-photo.jpg'
-          placeholder='blur'
-          width={100}
-          height={100}
-          priority
-          className='rounded-full'
-        />
-      </div>
-    </>
-  )
-}
+const Home = () => (
+  <>
+    <Head title='About Sha Mwe La' />
+    <p>
+      Hi, I'm Sha Mwe La. I write programs. I love listening to music and
+      watching movies.
+    </p>
+    <div className='self-start'>
+      <Image
+        alt='Sha Mwe La'
+        src='/images/sha-mwe-la-photo.jpg'
+        blurDataURL='/images/sha-mwe-la-photo.jpg'
+        placeholder='blur'
+        width={100}
+        height={100}
+        priority
+        className='rounded-full'
+      />
+    </div>
+  </>
+)
 
 export default Home
