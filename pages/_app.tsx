3501f6acc12d6614f7b406460b5468c9ed6c65b7
Sha Mwe La <shamwela2002@gmail.com>
2022-04-18 22:52:16 +0630

    Improve

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,14 @@ import { AppProps } from 'next/app'
 import Footer from 'components/Footer'
 import Navigation from 'components/Navigation'
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Navigation />
-      <main className='mx-auto flex max-w-[70ch] flex-col p-5'>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </>
-  )
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Navigation />
+    <main className='mx-auto flex max-w-[70ch] flex-col p-5'>
+      <Component {...pageProps} />
+    </main>
+    <Footer />
+  </>
+)
 
 export default App
