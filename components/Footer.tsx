d3f0cd21ed79666951a6030dbd4b7187ebe77d38
Sha Mwe La <shamwela2002@gmail.com>
2022-05-28 15:32:57 +0630

    Improve

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import ExternalLink from './ExternalLink'
 
 const Footer = () => (
   <footer className='mx-auto max-w-[70ch] flex flex-col gap-y-4 border-t-[1px] p-5'>
+    <Link href='/'>
+      <a>Blog</a>
+    </Link>
     <ExternalLink href='mailto:shamwela@hotmail.com'>Email</ExternalLink>
     <ExternalLink href='https://github.com/shamwela'>GitHub</ExternalLink>
     <ExternalLink href='https://www.linkedin.com/in/shamwela'>
