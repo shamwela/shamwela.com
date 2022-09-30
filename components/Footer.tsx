b50d983d3c785559a6329fb5f5d16af7c9b45c52
Sha Mwe La <shamwela2002@gmail.com>
2022-09-30 11:20:54 +0630

    Improve

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,17 +3,14 @@ import ExternalLink from './ExternalLink'
 
 const Footer = () => (
   <footer className='mx-auto max-w-[70ch] flex flex-col gap-y-4 border-t-[1px] p-5'>
-    <Link href='/projects'>
-      <a>Projects</a>
-    </Link>
     <Link href='/'>
       <a>Blog</a>
     </Link>
-    <ExternalLink href='mailto:shamwela2002@gmail.com'>Email</ExternalLink>
-    <ExternalLink href='https://github.com/shamwela'>GitHub</ExternalLink>
     <ExternalLink href='https://www.linkedin.com/in/shamwela'>
       LinkedIn
     </ExternalLink>
+    <ExternalLink href='mailto:shamwela2002@gmail.com'>Email</ExternalLink>
+    <ExternalLink href='https://github.com/shamwela'>GitHub</ExternalLink>
     <ExternalLink href='https://twitter.com/shamwela_'>Twitter</ExternalLink>
   </footer>
 )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import 'styles/globals.css'
 import { AppProps } from 'next/app'
 import Footer from 'components/Footer'
-import Navigation from 'components/Navigation'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
-    <Navigation />
     <main className='mx-auto flex max-w-[70ch] flex-col p-5'>
       <Component {...pageProps} />
     </main>
