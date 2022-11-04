384841d249509cbdb08195f0bd8f2d97c90c7012
Sha Mwe La <shamwela2002@gmail.com>
2022-11-04 21:05:43 +0630

    Change heading font to Pilat Wide Bold

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import 'styles/global.css'
 import { AppProps } from 'next/app'
 import Footer from 'components/Footer'
+import { pilatWideBoldFont } from 'utilities/pilatWideBoldFont'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
+    <style jsx global>{`
+      h1,
+      h2,
+      h3 {
+        font-family: ${pilatWideBoldFont.style.fontFamily};
+      }
+    `}</style>
     <main className='mx-auto flex max-w-[70ch] flex-col p-5'>
       <Component {...pageProps} />
     </main>
