0593d87d93e0af8b3d63990404a5ffb4eaaf46ca
Sha Mwe La <shamwela2002@gmail.com>
2022-11-04 21:04:18 +0630

    Re-add 404 and document

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import Head from 'components/Head'
+import Link from 'next/link'
+
+const Custom404 = () => (
+  <>
+    <Head title='Page not found' />
+
+    <div className='flex flex-col items-center gap-y-4'>
+      <h1>Page not found</h1>
+      <Link href='/'>
+        <a>Go to home page</a>
+      </Link>
+    </div>
+  </>
+)
+
+export default Custom404
