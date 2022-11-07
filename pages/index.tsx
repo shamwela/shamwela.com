c625ef31b565f19115a871379a06631057ff90d9
Sha Mwe La <shamwela2002@gmail.com>
2022-11-07 20:30:46 +0630

    Add SML Social live site URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,19 @@ const projects = [
     name: 'SML Eats',
     description: 'A food delivery app',
     gitHubUrl: 'https://github.com/shamwela/sml-eats',
-    liveUrl: 'https://smleats.vercel.app',
+    liveSiteUrl: 'https://smleats.vercel.app',
   },
   {
     name: 'SML Social',
     description: 'Facebook clone',
     gitHubUrl: 'https://github.com/shamwela/sml-social',
+    liveSiteUrl: 'https://smlsocial.up.railway.app',
   },
   {
     name: 'shamwela.com',
     description: 'My website',
     gitHubUrl: 'https://github.com/shamwela/shamwela.com',
-    liveUrl: 'https://www.shamwela.com',
+    liveSiteUrl: 'https://www.shamwela.com',
   },
 ]
 
@@ -27,12 +28,12 @@ export default function Portfolio() {
       <Head title="Sha Mwe La's portfolio" />
       <h1>Portfolio</h1>
       <div className='flex flex-col gap-y-4'>
-        {projects.map(({ name, description, gitHubUrl, liveUrl }) => (
+        {projects.map(({ name, description, gitHubUrl, liveSiteUrl }) => (
           <div key={name} className='flex flex-col gap-y-4'>
             <h2>{name}</h2>
             <p>{description}</p>
             <ExternalLink href={gitHubUrl}>GitHub</ExternalLink>
-            {liveUrl && <ExternalLink href={liveUrl}>Live site</ExternalLink>}
+            <ExternalLink href={liveSiteUrl}>Live site</ExternalLink>
           </div>
         ))}
       </div>
