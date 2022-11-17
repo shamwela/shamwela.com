100ac56ea0e53a952e485fe383f9bd0c21c682a6
Sha Mwe La <shamwela2002@gmail.com>
2022-11-17 20:06:27 +0630

    Add my image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'components/Head'
+import Image from 'next/image'
+import ShaMweLaImage from 'public/images/sha-mwe-la.jpg'
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <>
       <Head title='Sha Mwe La' />
@@ -9,6 +11,13 @@ export default function HomePage() {
         I'm a full-stack web developer based in Yangon, Myanmar. I'm studying
         Computing at GUSTO University.
       </p>
+      <Image
+        src={ShaMweLaImage}
+        alt='Sha Mwe La image'
+        width={200}
+        height={200}
+        placeholder='blur'
+      />
     </>
   )
 }
