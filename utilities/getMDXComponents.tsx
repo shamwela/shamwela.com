5ae500ceb5953206945d3111098f96d84252cdba
Sha Mwe La <shamwela2002@gmail.com>
2022-11-23 09:46:04 +0630

    Fix types

--- utilities/getMDXComponents.tsx
+++ utilities/getMDXComponents.tsx
@@ -4,30 +4,30 @@ import Link from 'next/link'
 import type { ImageProperty } from 'types/imageProperty'
 
 export const getMDXComponents = (imagesProperties: ImageProperty[]) => {
-  const CustomImage = ({ src, alt }: { src: string; alt: string }) => {
+  const CustomImage = ({ src, alt = '' }: { src?: string; alt?: string }) => {
     const imageProperties = imagesProperties.find(
       (properties) => properties.src === src
     )
-
-    if (imageProperties) {
-      return <Image {...imageProperties} placeholder='blur' alt={alt} />
-    } else {
+    if (!imageProperties) {
       return <div>(Image not found.)</div>
     }
+    return <Image {...imageProperties} placeholder='blur' alt={alt} />
   }
 
   const CustomLink = ({
     href,
     children,
   }: {
-    href: string
-    children: React.ReactNode
+    href?: string
+    children?: React.ReactNode
   }) => {
+    if (!href) {
+      return <>{children} (broken link)</>
+    }
     if (href.startsWith('http')) {
       return <ExternalLink href={href}>{children}</ExternalLink>
-    } else {
-      return <Link href={href}>{children}</Link>
     }
+    return <Link href={href}>{children}</Link>
   }
 
   return { img: CustomImage, a: CustomLink }
