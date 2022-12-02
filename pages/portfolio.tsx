edcaeb40e069b950170b26c98d351fb9d4347c6a
Sha Mwe La <shamwela2002@gmail.com>
2022-12-02 16:03:18 +0630

    Add tech stacks

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -10,6 +10,8 @@ const projects = [
     description: 'A food delivery app',
     gitHubUrl: 'https://github.com/shamwela/sml-eats',
     liveSiteUrl: 'https://smleats.vercel.app',
+    techStack:
+      'MySQL, Prisma, Express, Node.js, Next.js, React, Redux Toolkit, TypeScript, JavaScript, Tailwind CSS, CSS, Google Analytics, Sentry, Cypress',
     imageSource: SMLEatsImage,
   },
   {
@@ -17,6 +19,7 @@ const projects = [
     description: 'Facebook clone',
     gitHubUrl: 'https://github.com/shamwela/sml-social',
     liveSiteUrl: 'https://smlsocial.up.railway.app',
+    techStack: 'MySQL, Laravel, PHP, Blade, Tailwind CSS, Cloudinary',
     imageSource: SMLSocialImage,
   },
 ]
@@ -28,19 +31,33 @@ export default function Portfolio() {
       <h1>Portfolio</h1>
       <div className='flex flex-col gap-y-4'>
         {projects.map(
-          ({ name, description, gitHubUrl, liveSiteUrl, imageSource }) => (
+          ({
+            name,
+            description,
+            techStack,
+            gitHubUrl,
+            liveSiteUrl,
+            imageSource,
+          }) => (
             <div key={name} className='flex flex-col gap-y-4'>
               <h2>{name}</h2>
-              <p>
-                {description} (
+              <span>
                 <ExternalLink href={gitHubUrl}>GitHub</ExternalLink>,{' '}
-                <ExternalLink href={liveSiteUrl}>Live site</ExternalLink>)
-              </p>
-              <Image src={imageSource} alt={name} placeholder='blur' />
+                <ExternalLink href={liveSiteUrl}>Live site</ExternalLink>
+              </span>
+              <span>{description}</span>
+              <span>Tech stack: {techStack}</span>
+              <ExternalLink href={liveSiteUrl}>
+                <Image src={imageSource} alt={name} placeholder='blur' />
+              </ExternalLink>
             </div>
           )
         )}
       </div>
+      <span>
+        See more projects on my{' '}
+        <ExternalLink href='https://github.com/shamwela'>GitHub</ExternalLink>.
+      </span>
     </>
   )
 }
